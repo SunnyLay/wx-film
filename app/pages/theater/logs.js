2be59cfb678712a0feccf630aa42ef9844ad4b45
wanghaichao <m18888106873@163.com>
2018-03-19 09:27:37 +0800

    add 影院下拉刷新

--- app/pages/theater/logs.js
+++ app/pages/theater/logs.js
@@ -54,6 +54,40 @@ Page({
       }
     })
   },
+
+  onReachBottom: function () {
+    var that = this
+    wx.request({
+      url: getApp().data.newHost + '/film/getCinemas/'
+      + that.data.lat + '/'
+      + that.data.lng + '/'
+      + that.data.page + '/'
+      + that.data.size,
+      success: function (res) {
+        var theaterLi = that.data.theater
+        var nextLi = res.data.content;
+        console.info(nextLi);
+        var newLi = theaterLi.concat(nextLi)
+        if (nextLi.length == 0) {
+          wx.showToast({
+            title: '暂无更多数据...',
+            icon: 'none',
+            duration: 2000
+          })
+        } else {
+          wx.showToast({
+            title: '加载中',
+            icon: 'loading',
+            duration: 1000
+          })
+          that.setData({
+            theater: newLi,
+            page: that.data.page + 1
+          })
+        }
+      }
+    })
+  },
   onShow: function () {
     var that = this
     app.getpol(function (currentCity) {
